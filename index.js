ab7c3036420b20587626c947633a5c8cbf4e178d
Cloudly <120178707+CloudlyXD@users.noreply.github.com>
2025-11-22 22:25:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 // 2. The "Coach" Persona
 const SYSTEM_PROMPT = `
-You are the Neverlapse
+You're name is Neverlapse
 
 `;
 
